e1eac7cbd4f63fa7752627e8a6e2dc5ee5c31724
Zenext <artjomvinnikov@gmail.com>
2016-08-10 21:25:57 +0300

    Generate board by number of givens to be kept

--- src/generator/Generator.js
+++ src/generator/Generator.js
@@ -1,30 +1,53 @@
 import {solve} from '../solver/Solver';
+import Cell from '../Cell';
+import {randomValue, randomFromArray, shuffle} from '../utils';
 
-let _board;
-
-function generate(width, height) {
+function generate(givens) {
 	//simply run solver on an empty board
-	_board = solve(createEmptyBoard());
-	dig();
+	const {board} = solve(createEmptyBoard(), false);
+	const finalBoard = dig(board, 32);
+
+	return finalBoard;
+};
+
+function dig(board, givens) {
+	const cells = shuffle(getAllCells());
+	let cell, solvable, value;
+	let removed = 0;
+
+	board = board.clone();
+
+	for (let i = 0; i < cells.length; i++) {
+		if (removed >= cells.length - givens) {
+			break;
+		}
+
+		cell = cells[i];
+		value = board[cell.x][cell.y];
+		board[cell.x][cell.y] = 0;
+
+		solvable = solve(board.clone(), true).solvable;
+
+		if (!solvable) {
+			board[cell.x][cell.y] = value;
+			continue;
+		}
+
+		removed++;
+	}
 
 	return board;
 };
 
-function dig() {
-	const {x, y} = getFilledCell();	
-	_board[x][y] = 0;
+function getAllCells() {
+	const arr = [];
+	for (let x = 0; x < 9; x++) {
+		for (let y = 0; y < 9; y++) {
+			arr.push({x, y});
+		}
+	}
 
-
-};
-
-function getFilledCell() {
-	const x = randomValue(1, 9);
-	const y = randomValue(1, 9);
-
-	if (_board[x][y] === 0)
-		getFilledCell(_board);
-
-	return {x, y};
+	return arr;
 };
 
 function createEmptyBoard() {
@@ -38,7 +61,7 @@ function createEmptyBoard() {
 	}
 
 	return board;
-};
+}
 
 export {
 	generate
